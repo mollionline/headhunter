2e5463401db13587c91ed036bca504cddf0a19bc
moldir <@auezova_moldir>
2022-04-16 13:48:50 +0600

    Отредактировала редактирование вакансии и условие опубликовать вакансию или нет, настроила выводы данных в главной стр и на странице пользователя

--- headhunter/urls.py
+++ headhunter/urls.py
@@ -5,10 +5,10 @@ from headhunter.views import vacancy_views
 urlpatterns = []
 
 vacancy_urls = [
-    path('vacancy/list/', vacancy_views.VacancyListView.as_view(), name='vacancy_list'),
+    path('', vacancy_views.VacancyListView.as_view(), name='vacancy_list'),
     path('vacancy/<int:pk>/', vacancy_views.VacancyDetailsView.as_view(), name='vacancy_details'),
     path('vacancy/create/', vacancy_views.VacancyCreateView.as_view(), name='vacancy_create'),
-    path('vacancy/update/<int:pk>/', vacancy_views.VacancyUpdateView.as_view(), name='vacancy_update'),
+    path('vacancy/<int:pk>/update', vacancy_views.VacancyUpdateView.as_view(), name='vacancy_update'),
     path('vacancy/delete/<int:pk>/', vacancy_views.VacancyDeleteView.as_view(), name='vacancy_delete'),
 ]
 
--- headhunter/views/vacancy_views.py
+++ headhunter/views/vacancy_views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 
 from headhunter.forms.vacancy_forms import VacancyForm
-from headhunter.models import Vacancy
+from headhunter.models import Vacancy, Resume, CATEGORIES
 from headhunter.helpers import FormView as CustomFormView
 
 
@@ -14,6 +14,10 @@ class VacancyListView(ListView):
     ordering = ('-updated_at',)
     context_object_name = 'vacancies'
     paginate_by = 6
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        kwargs['resumes'] = Resume.objects.all()
+        return super().get_context_data(**kwargs)
 
 
 class VacancyDetailsView(DetailView):
@@ -30,23 +34,37 @@ class VacancyCreateView(LoginRequiredMixin, CustomFormView):
     form_class = VacancyForm
     redirect_url = ''
 
+    def get(self, request, *args, **kwargs):
+        form = VacancyForm()
+        categories = CATEGORIES
+        return render(request, self.template_name,
+                      context={'form': form,
+                               'categories': categories})
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(data=request.POST)
+        categories = CATEGORIES
         if form.is_valid():
             vacancy = form.save(commit=False)
+            user = request.user
+            vacancy.author = user
             vacancy.save()
-            return redirect('vacancy_list')
+            return redirect('/')
         return render(request, self.template_name,
                       context={
-                          'form': form
-                      }
-                      )
+                          'form': form,
+                          'categories': categories
+                      })
 
 
 class VacancyUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'vacancy/vacancy_update.html'
     form_class = VacancyForm
     model = Vacancy
+
+    def get_context_data(self, **kwargs):
+        kwargs['categories'] = CATEGORIES
+        return super(VacancyUpdateView, self).get_context_data(**kwargs)
 
     def get_success_url(self):
         return reverse('vacancy_details', kwargs={'pk': self.get_object().pk})
