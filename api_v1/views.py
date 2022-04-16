724b35d141aa1c103a720a11e7db1b61c46f053f
Bakhytbek <baxa.aseke@gmail.com>
2022-04-16 13:09:17 +0600

    Создал apiview для выхода и для добавления стажа

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from api_v1.serializers import ExperienceSerializer
 
-# Create your views here.
+
+class LogoutView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'ok'})
+
+
+class ExperienceCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = ExperienceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
