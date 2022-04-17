870812b4ccf38e80a0fa799acc927aaf57050646
Bakhytbek <baxa.aseke@gmail.com>
2022-04-17 12:19:16 +0600

    Добавил url для GRUD резюме

--- headhunter/urls.py
+++ headhunter/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from headhunter.views import vacancy_views
-from headhunter.views.resume_views import ResumeCreateView, ResumeDetailView
+from headhunter.views.resume_views import (
+    ResumeCreateView, ResumeDetailView, ResumeDeleteView, ResumeUpdateView,
+    ExperienceUpdateView, ExperienceDeleteView, EducationUpdateView, EducationDeleteView,
+    ExperienceCreateView, EducationCreateView
+)
 
 urlpatterns = []
 
@@ -16,8 +20,16 @@ vacancy_urls = [
 urlpatterns += vacancy_urls
 
 resume_urls = [
-    path('resume/create', ResumeCreateView.as_view(), name='create_resume'),
-    path('resume/<int:pk>/', ResumeDetailView.as_view(), name='detail_resume')
+    path('resume/create/', ResumeCreateView.as_view(), name='create_resume'),
+    path('resume/<int:pk>/', ResumeDetailView.as_view(), name='detail_resume'),
+    path('resume/delete/<int:pk>/', ResumeDeleteView.as_view(), name='delete_resume'),
+    path('resume/update/<int:pk>/', ResumeUpdateView.as_view(), name='update_resume'),
+    path('experience/update/<int:pk>/<int:resume>/', ExperienceUpdateView.as_view(), name='update_experience'),
+    path('experience/delete/<int:pk>/<int:resume>/', ExperienceDeleteView.as_view(), name='delete_experience'),
+    path('education/update/<int:pk>/<int:resume>/', EducationUpdateView.as_view(), name='update_education'),
+    path('education/delete/<int:pk>/<int:resume>/', EducationDeleteView.as_view(), name='delete_education'),
+    path('experience/create/<int:pk>/', ExperienceCreateView.as_view(), name='experience_create'),
+    path('education/create/<int:pk>/', EducationCreateView.as_view(), name='create_education')
 ]
 
 urlpatterns += resume_urls
