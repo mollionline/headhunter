456a77eed9f37c1c237b3ee504cb5697b69d7a40
Bakhytbek <baxa.aseke@gmail.com>
2022-04-17 12:18:30 +0600

    Добавил форму для добавления стажа и для добавления образования

--- headhunter/forms/resume_forms.py
+++ headhunter/forms/resume_forms.py
@@ -1,12 +1,33 @@
 from django import forms
 
-from headhunter.models import Resume
+from headhunter.models import Resume, Experiences, Educations
 
 
 class ResumeForm(forms.ModelForm):
-    about_me = forms.TimeField(required=False)
+    about_me = forms.CharField(required=False)
     salary = forms.IntegerField(required=False)
 
     class Meta:
         model = Resume
         exclude = ['user']
+
+
+class ResumeUpdateForm(forms.ModelForm):
+    about_me = forms.CharField(required=False)
+    salary = forms.IntegerField(required=False)
+
+    class Meta:
+        model = Resume
+        exclude = ['user', 'work_experience', 'education']
+
+
+class ExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Experiences
+        fields = ['name_company', 'start_date', 'end_date', 'position', 'responsibilities']
+
+
+class EducationUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Educations
+        fields = ['institution_name', 'start_date', 'end_date', 'speciality']
