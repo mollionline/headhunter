1efc594baadbbd1d8ae7922d50d6297bf534d96d
moldir <@auezova_moldir>
2022-04-18 12:57:12 +0600

    #13 Создала дамп и uploads откорректировала некоторые моменты шаблоне убрала лишние коды

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from accounts.models import Profile
 # Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['user', 'email', 'phone', 'avatar', 'user_or_company']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline, ]
+
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
